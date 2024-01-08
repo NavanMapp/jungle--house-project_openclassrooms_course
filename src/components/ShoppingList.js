0fa42feedf52aab59ece51c8ff05174058bad9e8
NavanMapp <navanmapp@gmail.com>
2024-01-08 15:01:55 +0200

    ShoppingList update

---
+++ src/components/ShoppingList.js
@@ -0,0 +1,26 @@
+import {plantList} from '../datas/plantList'
+
+export default function ShoppingList() {
+    const categories = plantList.reduce((cat, plant) => {
+        if (!cat.includes(plant.category)) {
+            cat.push(plant.category);
+        }
+        return cat;
+    }, [])
+
+    return (
+        <div> 
+            <h2> Plant Categories: </h2>
+            <ul>
+                {categories.map((plant, index) => (
+                    <li key={`${plant}-${index}`}>{plant}</li>
+                ))}
+            </ul>
+            <ul>
+                {plantList.map((plant, index) => (
+                    <li key={`${plant.name}-${index}`}>{plant.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
