cc28429c0b03bea35bbf3e2202e719fe983a04b9
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2024-01-08 19:44:31 +0200

    plantList update

---
+++ src/datas/plantList.js
@@ -0,0 +1,65 @@
+export const plantList = [
+    {
+        name: 'monstera',
+        category: 'classic',
+        id: '1ed',
+        isBestSale: true,
+        isSpecialOffer: false
+    },
+    {
+        name: 'ficus lyrata',
+        category: 'classic',
+        id: '2ab',
+        isBestSale: false,
+        isSpecialOffer: true
+    },
+    {
+        name: 'silver pothos',
+        category: 'classic',
+        id: '3sd',
+        isBestSale: false,
+        isSpecialOffer: false
+    },
+    {
+        name: 'yucca',
+        category: 'classic',
+        id: '4kk',
+        isBestSale: false,
+        isSpecialOffer: true
+    },
+    {
+        name: 'olive tree',
+        category: 'exterior',
+        id: '5pl',
+        isBestSale: false,
+        isSpecialOffer: false
+    },
+    {
+        name: 'geranium',
+        category: 'exterior',
+        id: '6uo',
+        isBestSale: false,
+        isSpecialOffer: false
+    },
+    {
+        name: 'basil',
+        category: 'exterior',
+        id: '7ie',
+        isBestSale: true,
+        isSpecialOffer: false
+    },
+    {
+        name: 'aloe',
+        category: 'succulent',
+        id: '8fp',
+        isBestSale: false,
+        isSpecialOffer: true
+    },
+    {
+        name: 'succulent',
+        category: 'succulent',
+        id: '9vn',
+        isBestSale: false,
+        isSpecialOffer: true
+    }
+]
