61fc04877b3dc159661e0e273a41f4adc3d8573a
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2024-01-08 20:13:25 +0200

    shoppinglist update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Banner from './components/Banner.js';
 import Cart from './components/Cart';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
   <div>
       <Banner />
       <Cart />
+      <ShoppingList />
   </div>
   )
 }
