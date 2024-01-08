e395535efea93a36b8c39c7a48a5014fa09f81b9
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2024-01-08 13:19:44 +0200

    Jungle house project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from './components/Banner.js';
+import Cart from './components/Cart';
+
+function App() {
+  return (
+  <div>
+      <Banner />
+      <Cart />
+  </div>
+  )
+}
+
+export default App;
