36fedcd8f33a6b6d9fedc0b37833338a1a7df6dc
NavanMapp <navanmapp@gmail.com>
2024-01-09 09:24:46 +0200

    PlantItem update

---
+++ src/components/PlantItem.js
@@ -0,0 +1,16 @@
+import CareScale from './CareScale'
+
+export default function PlantItem( {id, cover, name, water, light }) {
+    return (
+        <div>
+            <li key={id} className='jh-plant-item'>
+                <img className='jh=plant-item-cover' src={cover} alt={`${name} cover`} />
+                {name}
+                <div>
+                    <CareScale careType='water' scaleValue={water} />
+                    <CareScale careType='light' scaleValue={light} />
+                </div>
+            </li>
+        </div>
+    )
+}
