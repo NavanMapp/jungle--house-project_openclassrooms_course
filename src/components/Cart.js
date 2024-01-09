86c0d76b435f97abdb0719a88e113b57f37b4df2
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2024-01-09 19:53:17 +0200

    updating files

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,19 @@
 import '../styles/Cart.css'
+import { useState } from 'react'
 
 export default function Cart() {
+    const [cart, updateCart] = useState(0)
     const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
+
     return (
     <div className='jh-cart'>
         <h2>Cart</h2>
-        <ul>
-            <li>Montsera: {monsteraPrice}€</li>
-            <li>Ivy: {ivyPrice}€</li>
-            <li>Flowers: {flowerPrice}€</li>
-        </ul>
-        Total: {monsteraPrice + ivyPrice + flowerPrice}€
+        <div>
+            Montsera: {monsteraPrice}€
+            <button onClick={() => updateCart(cart + 1)}>
+                Add
+            </button>
+        </div>
+        <h3>Total: {monsteraPrice * cart}€</h3>
     </div>)
 }
