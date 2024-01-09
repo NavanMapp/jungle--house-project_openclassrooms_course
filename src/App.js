ec9871672af4b5bb2eed337ebd9a8dc62d9aa8a8
NavanMapp <navanmapp@gmail.com>
2024-01-09 14:40:50 +0200

    components updated

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ import Footer from './components/Footer.js';
 import ShoppingList from './components/ShoppingList';
 
 export default function App() {
-  function handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault()
     alert(e.target['my_input'].value)
   }
+
   return (
   <div>
       <Banner />
       <Cart />
       <ShoppingList />
       <Footer />
-      {/* <form onSubmit={handleSubmit}>
-        <input type='text' name='my_input' defaultValue='Type your text' />
-        <button type='submit'>Enter</button>
-      </form> */}
   </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,11 @@ import '../styles/Footer.css'
 export default function Footer() {
     const [inputValue, setInputValue] = useState('')
 
-    function handleSubmit(e) {
+    const handleInput = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const handleBlur = (e) => {
         e.preventDefault()
         if (!inputValue.includes('@')) {
             alert(e.target['Error: no @ has been entered. This is not a valid email address.'].value)
@@ -12,15 +16,20 @@ export default function Footer() {
     }
 
     return(
-        <Footer className='jh-footer'>
+        <footer className='jh-footer'>
             <div className='jh-footer-elem'>
                 For plant enthusiasts 🌿🌱🌵
             </div>
             <div className='jh-footer-elem'>Subscribe to our newsletter : </div>
-            <form onSubmit={handleSubmit}>
-                <input type='text' name='my_input' defaultValue='Enter Your Email Address Here' />
+            <form onSubmit={handleBlur}>
+                <input 
+                    placeholder='Enter Your Email Address' 
+                    onChange={handleInput}
+                    value={inputValue}
+                    onBlur={handleBlur}  
+                />
                 <button type='submit'>Enter</button>
             </form>
-        </Footer>
+        </footer>
     )
 }
