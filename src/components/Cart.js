30484330d482ae4f1302856c9d08cf00031cec03
NavanMapp <navanmapp@gmail.com>
2024-01-10 13:47:33 +0200

    Updating Cart files

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,16 +4,38 @@ import { useState } from 'react'
 export default function Cart() {
     const [cart, updateCart] = useState(0)
     const monsteraPrice = 8
+    const [isOpen, setIsOpen] = useState(true)
 
-    return (
+    const clearCart = () => {
+        updateCart(0)
+    }
+
+    return isOpen ? (
     <div className='jh-cart'>
+        <button
+            className='jh-cart-toggle-button'
+            onClick={() => setIsOpen(false)}
+        >
+            Close    
+        </button>
         <h2>Cart</h2>
         <div>
-            Montsera: {monsteraPrice}€
+            Montsera: R{monsteraPrice}
             <button onClick={() => updateCart(cart + 1)}>
                 Add
             </button>
         </div>
-        <h3>Total: {monsteraPrice * cart}€</h3>
-    </div>)
+        <h3>Total: R{monsteraPrice * cart}</h3>
+        <button
+            onClick={clearCart} className='jh-clear-cart-button'
+        >
+            Clear Cart
+        </button>
+    </div>) : (
+        <button
+            className='jh-cart-toggle-button'
+            onClick={() => setIsOpen(true)}>
+            Open Cart
+        </button>
+    )
 }
